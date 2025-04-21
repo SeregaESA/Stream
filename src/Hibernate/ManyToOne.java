5819d49fa9f5065ee5cfc5e1c42252c64611c486
SeregaESA <esa_scb@mail.ru>
2025-04-21 19:12:23 +0300

    generic

---
+++ src/Hibernate/ManyToOne.java
@@ -0,0 +1,28 @@
+package Hibernate;
+
+public class ManyToOne {
+
+// @ManyToOne (optional = true/false, fetch = FetchType.LAZY/EAGER, cascade = CascadeType.DETACH/PERSIST)
+// @JoinColumn(name = "company_id")
+// private Company companyId;
+//
+// optional = true/false:
+//       1. true - left outer join
+//       2. false - inner join
+//
+// fetch = FetchType.LAZY/EAGER:
+//       1. EAGER - жадная загрузка
+//       2. LAZY - ленивая загрузка (использует proxy объект)
+//
+// cascade = CascadeType.DETACH/PERSIST:
+//       1. DETACH - при удалении сущности из persistenceContext, удалит из persistenceContext и сущность к которой
+//                   имеет связь
+//       2. PERSIST -
+//
+//
+//
+//
+//
+//
+
+}
